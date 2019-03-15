cf670bfedb77420afcdccecc539a6e5839d3b052
kodofish Chang <kodofish@gmail.com>
2019-03-15 14:13:49 +0800

    增加一個新的測試

--- CSharpAdvanceDesignTests/JoeyWhereTests.cs
+++ CSharpAdvanceDesignTests/JoeyWhereTests.cs
@@ -66,6 +66,41 @@ namespace CSharpAdvanceDesignTests
         }
                
         [Test]
+        public void find_products_that_price_between_200_and_500_and_cost_more_than_30_use_IEnumerable()
+        {
+            var products = new List<Product>
+            {
+                new Product {Id = 1, Cost = 11, Price = 110, Supplier = "Odd-e"},
+                new Product {Id = 2, Cost = 21, Price = 210, Supplier = "Yahoo"},
+                new Product {Id = 3, Cost = 31, Price = 310, Supplier = "Odd-e"},
+                new Product {Id = 4, Cost = 41, Price = 410, Supplier = "Odd-e"},
+                new Product {Id = 5, Cost = 51, Price = 510, Supplier = "Momo"},
+                new Product {Id = 6, Cost = 61, Price = 610, Supplier = "Momo"},
+                new Product {Id = 7, Cost = 71, Price = 710, Supplier = "Yahoo"},
+                new Product {Id = 8, Cost = 18, Price = 780, Supplier = "Yahoo"}
+            };
+
+            var actual = products
+                .FishWhere(product => product.Price>200 && product.Price<500 && product.Cost > 30)
+                .JoeySelect(p=>p.Price);
+
+            var expected = new []
+            {
+                310,
+                410
+            };
+            
+//            var expected = new List<Product>
+//            {
+//                new Product {Id = 3, Cost = 31, Price = 310, Supplier = "Odd-e"},
+//                new Product {Id = 4, Cost = 41, Price = 410, Supplier = "Odd-e"}
+//            };
+
+            expected.ToExpectedObject().ShouldMatch(actual);
+        }
+
+        
+        [Test]
         public void find_odd_names()
         {
             var names = new List<string> {"Joey", "Cash", "William", "Sam", "Brian", "Jessica"};

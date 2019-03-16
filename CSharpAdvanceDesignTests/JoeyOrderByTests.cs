2dbd46f769748cea40709c6d7b43186625aeab38
kodofish Chang <kodofish@gmail.com>
2019-03-16 14:59:20 +0800

    - 變更需求，如果 LastName 比不出來就比 FirstName

--- CSharpAdvanceDesignTests/JoeyOrderByTests.cs
+++ CSharpAdvanceDesignTests/JoeyOrderByTests.cs
@@ -11,7 +11,7 @@ namespace CSharpAdvanceDesignTests
     [TestFixture]
     public class JoeyOrderByTests
     {
-        [Test]
+        [Ignore("temp")]
         public void orderBy_lastName()
         {
             var employees = new[]
@@ -35,6 +35,30 @@ namespace CSharpAdvanceDesignTests
             expected.ToExpectedObject().ShouldMatch(actual);
         }
 
+        [Test]
+        public void orderBy_lastName_and_firstName()
+        {
+            var employees = new[]
+            {
+                new Employee {FirstName = "Joey", LastName = "Wang"},
+                new Employee {FirstName = "Tom", LastName = "Li"},
+                new Employee {FirstName = "Joseph", LastName = "Chen"},
+                new Employee {FirstName = "Joey", LastName = "Chen"},
+            };
+
+            var actual = JoeyOrderByLastName(employees);
+
+            var expected = new[]
+            {
+                new Employee {FirstName = "Joey", LastName = "Chen"},
+                new Employee {FirstName = "Joseph", LastName = "Chen"},
+                new Employee {FirstName = "Tom", LastName = "Li"},
+                new Employee {FirstName = "Joey", LastName = "Wang"},
+            };
+
+            expected.ToExpectedObject().ShouldMatch(actual);
+        }
+
         private IEnumerable<Employee> JoeyOrderByLastName(IEnumerable<Employee> employees)
         {
             //bubble sort
@@ -46,7 +70,9 @@ namespace CSharpAdvanceDesignTests
                 var index = 0;
                 for (int i = 1; i < elements.Count; i++)
                 {
-                    if (stringComparer.Compare(elements[i].LastName, minElement.LastName) < 0)
+                    if ((stringComparer.Compare(elements[i].LastName, minElement.LastName) == 0
+                        && stringComparer.Compare(elements[i].FirstName, minElement.FirstName) < 0) || 
+                        stringComparer.Compare(elements[i].LastName, minElement.LastName) < 0)
                     {
                         minElement = elements[i];
                         index = i;

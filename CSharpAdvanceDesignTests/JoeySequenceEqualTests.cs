a51ef4c66a98a438ca9ca37f88d37b64be44ed50
kodofish Chang <kodofish@gmail.com>
2019-03-16 11:33:10 +0800

    - Implement JoeySequence method and 3 test

--- CSharpAdvanceDesignTests/JoeySequenceEqualTests.cs
+++ CSharpAdvanceDesignTests/JoeySequenceEqualTests.cs
@@ -18,17 +18,53 @@ namespace CSharpAdvanceDesignTests
             Assert.IsTrue(actual);
         }
 
+        [Test]
+        public void compare_two_numbers_sequence_not_equal()
+        {
+            var first = new List<int> { 3, 2, 1, 0 };
+            var second = new List<int> { 3, 2, 1 };
+
+            var actual = JoeySequenceEqual(first, second);
+
+            Assert.IsFalse(actual);
+        }
+
+        [Test]
+        public void compare_two_numbers_sequence_diff_not_equal()
+        {
+            var first = new List<int> { 3, 1, 2 };
+            var second = new List<int> { 3, 2, 1 };
+
+            var actual = JoeySequenceEqual(first, second);
+
+            Assert.IsFalse(actual);
+        }
+
+        [Test]
+        public void compare_two_numbers_sequence_all_empty__equal()
+        {
+            var first = new List<int> {  };
+            var second = new List<int> {  };
+
+            var actual = JoeySequenceEqual(first, second);
+
+            Assert.IsTrue(actual);
+        }
 
         private bool JoeySequenceEqual(IEnumerable<int> first, IEnumerable<int> second)
         {
             var firstEnumerator = first.GetEnumerator();
             var secondEnumerator = second.GetEnumerator();
-//            while (firstEnumerator.MoveNext() && secondEnumerator.MoveNext())
-//            {
-//                
-//            }
-
-            return true;
+            while (true)
+            {
+                var firstEmpty = !firstEnumerator.MoveNext();
+                var secondEmpty = !secondEnumerator.MoveNext();
+                
+                if (firstEmpty && secondEmpty) return true;
+                if (firstEmpty || secondEmpty) return false;
+                
+                if (firstEnumerator.Current != secondEnumerator.Current) return false;
+            }
         }
     }
 }

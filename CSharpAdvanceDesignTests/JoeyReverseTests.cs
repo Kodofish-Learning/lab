9aef896b5ae2f548517f326a14c4a93feb67e2e0
kodofish Chang <kodofish@gmail.com>
2019-03-16 10:30:27 +0800

    cleanup

--- CSharpAdvanceDesignTests/JoeyReverseTests.cs
+++ CSharpAdvanceDesignTests/JoeyReverseTests.cs
@@ -1,9 +1,8 @@
-﻿using ExpectedObjects;
+﻿using System.Collections.Generic;
+using ExpectedObjects;
 using Lab.Entities;
-using NUnit.Framework;
-using System.Collections.Generic;
-using System.Linq;
 using Lab.Extensions;
+using NUnit.Framework;
 
 namespace CSharpAdvanceDesignTests
 {
@@ -15,18 +14,18 @@ namespace CSharpAdvanceDesignTests
         {
             var employees = new List<Employee>
             {
-                new Employee(){FirstName = "Joey",LastName = "Chen"},
-                new Employee(){FirstName = "Tom",LastName = "Li"},
-                new Employee(){FirstName = "David",LastName = "Wang"},
+                new Employee {FirstName = "Joey", LastName = "Chen"},
+                new Employee {FirstName = "Tom", LastName = "Li"},
+                new Employee {FirstName = "David", LastName = "Wang"}
             };
 
             var actual = MyOwnLinq.JoeyReverse(employees);
 
             var expected = new List<Employee>
             {
-                new Employee(){FirstName = "David",LastName = "Wang"},
-                new Employee(){FirstName = "Tom",LastName = "Li"},
-                new Employee(){FirstName = "Joey",LastName = "Chen"},
+                new Employee {FirstName = "David", LastName = "Wang"},
+                new Employee {FirstName = "Tom", LastName = "Li"},
+                new Employee {FirstName = "Joey", LastName = "Chen"}
             };
 
             expected.ToExpectedObject().ShouldMatch(actual);

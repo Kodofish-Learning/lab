1076d9347e2ee545805196bb8621236484c79f9a
kodofish Chang <kodofish@gmail.com>
2019-03-17 14:04:33 +0800

    - demo validator 的使用場景

--- CSharpAdvanceDesignTests/JoeyOfTypeTests.cs
+++ CSharpAdvanceDesignTests/JoeyOfTypeTests.cs
@@ -11,6 +11,23 @@ namespace CSharpAdvanceDesignTests
     public class JoeyOfTypeTests
     {
         [Test]
+        public void get_special_type_value_from_arguments_2()
+        {
+            //ActionExecutingContext.ActionArguments: Dictionary<string,object>
+
+            var arguments = new Dictionary<string, object>
+            {
+                {"model", new Product {Price = 100, Cost = 111}},
+                {"validator", new ProductValidator()},
+            };
+
+            var validators = JoeyOfType<IValidator<Product>>(arguments.Values);
+
+            var products = JoeyOfType<Product>(arguments.Values).Single();
+            var isValid = validators.All(x => x.Validate(products));
+            Assert.IsFalse(isValid);
+        }
+        [Test]
         public void get_special_type_value_from_arguments()
         {
             //ActionExecutingContext.ActionArguments: Dictionary<string,object>
@@ -21,14 +38,37 @@ namespace CSharpAdvanceDesignTests
                 {"validator", new ProductValidator()},
             };
 
-            var validators = JoeyOfType<IValidator<Product>>(arguments);
+            var validators = JoeyOfType<IValidator<Product>>(arguments.Values);
 
             Assert.AreEqual(1, validators.Count());
         }
+        
+        [Test]
+        public void get_special_type_value_from_arguments_1()
+        {
+            //ActionExecutingContext.ActionArguments: Dictionary<string,object>
 
-        private IEnumerable<TResult> JoeyOfType<TResult>(Dictionary<string, object> arguments)
-        {   
-            throw new System.NotImplementedException();
+            var arguments = new Dictionary<string, object>
+            {
+                {"model", new Product {Price = 100, Cost = 111}},
+                {"validator", new ProductValidator()},
+                {"validator2", new ProductValidator()},
+            };
+
+            var validators = JoeyOfType<IValidator<Product>>(arguments.Values);
+
+            Assert.AreEqual(2, validators.Count());
+        }
+
+        private IEnumerable<TResult> JoeyOfType<TResult>(Dictionary<string, object>.ValueCollection arguments)
+        {
+            var sourceEnumerator = arguments.GetEnumerator();
+            while (sourceEnumerator.MoveNext())
+            {
+                var current = sourceEnumerator.Current;
+                if (current is TResult item)
+                    yield return item;
+            }
         }
     }
 }

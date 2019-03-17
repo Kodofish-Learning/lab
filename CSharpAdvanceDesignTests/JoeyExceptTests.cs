eea3dfffc9e79e52e776fbae4003fd2c5cf1ad8a
kodofish Chang <kodofish@gmail.com>
2019-03-17 11:11:38 +0800

    -Refactoring expect and fix bug

--- CSharpAdvanceDesignTests/JoeyExceptTests.cs
+++ CSharpAdvanceDesignTests/JoeyExceptTests.cs
@@ -11,7 +11,7 @@ namespace CSharpAdvanceDesignTests
         [Test]
         public void except_numbers()
         {
-            var first = new[] { 1, 3, 5, 7 };
+            var first = new[] { 1, 3, 5, 7, 3 };
             var second = new[] { 7, 1, 4 };
 
             var actual = JoeyExcept(first, second);
@@ -22,7 +22,6 @@ namespace CSharpAdvanceDesignTests
 
         private IEnumerable<int> JoeyExcept(IEnumerable<int> first, IEnumerable<int> second)
         {
-            var firstEnumerator = first.GetEnumerator();
             var hashSet = new HashSet<int>();
             
             var secondEnumerator = second.GetEnumerator();
@@ -31,10 +30,11 @@ namespace CSharpAdvanceDesignTests
                 hashSet.Add(secondEnumerator.Current);
             }
             
+            var firstEnumerator = first.GetEnumerator();
             while (firstEnumerator.MoveNext())
             {
                 var current = firstEnumerator.Current;
-                if (!hashSet.Remove(current))
+                if (hashSet.Add(current))
                 {
                     yield return current;
                 }

0ad196fc173f8621f76feb00cf6ec3fe0dafbd97
kodofish Chang <kodofish@gmail.com>
2019-03-17 16:03:43 +0800

    - Refactoring : JoeyJoin

--- CSharpAdvanceDesignTests/JoeyJoinTests.cs
+++ CSharpAdvanceDesignTests/JoeyJoinTests.cs
@@ -13,6 +13,43 @@ namespace CSharpAdvanceDesignTests
         [Test]
         public void all_pets_and_owner()
         {
+            var david = new Employee { FirstName = "David", LastName = "Li" };
+            var joey = new Employee { FirstName = "Joey", LastName = "Chen" };
+            var tom = new Employee { FirstName = "Tom", LastName = "Wang" };
+
+            var employees = new[]
+            {
+                david,
+                joey,
+                tom
+            };
+
+            var pets = new Pet[]
+            {
+                new Pet() {Name = "Lala", Owner = joey},
+                new Pet() {Name = "Didi", Owner = david},
+                new Pet() {Name = "Fufu", Owner = tom},
+                new Pet() {Name = "QQ", Owner = joey},
+            };
+
+            var actual = JoeyJoin<string>(employees, pets, 
+                employee1 => employee1, 
+                pet1 => pet1, (employee, pet) => $"{pet.Name} {employee.LastName}");
+
+            var expected = new[]
+            {
+                "Didi Li",
+                "Lala Chen",
+                "QQ Chen",
+                "Fufu Wang"
+            };
+
+            expected.ToExpectedObject().ShouldMatch(actual);
+        }
+
+        [Test]
+        public void all_pets_and_owner_and_get_result()
+        {
             var david = new Employee { FirstName = "David", LastName = "Chen" };
             var joey = new Employee { FirstName = "Joey", LastName = "Chen" };
             var tom = new Employee { FirstName = "Tom", LastName = "Chen" };
@@ -32,7 +69,7 @@ namespace CSharpAdvanceDesignTests
                 new Pet() {Name = "QQ", Owner = joey},
             };
 
-            var actual = JoeyJoin(employees, pets);
+            var actual = JoeyJoin<Tuple<string, string>>(employees, pets, employee1 => employee1, pet1 => pet1, (employee, pet) => Tuple.Create(employee.FirstName, pet.Name));
 
             var expected = new[]
             {
@@ -45,7 +82,9 @@ namespace CSharpAdvanceDesignTests
             expected.ToExpectedObject().ShouldMatch(actual);
         }
 
-        private IEnumerable<Tuple<string, string>> JoeyJoin(IEnumerable<Employee> employees, IEnumerable<Pet> pets)
+        private IEnumerable<TResult> JoeyJoin<TResult>(IEnumerable<Employee> employees,
+            IEnumerable<Pet> pets, Func<Employee, Employee> employeeKeySelector, Func<Pet, Pet> petKeySelector,
+            Func<Employee, Pet, TResult> resultSelector)
         {
             var employeesEnumerator = employees.GetEnumerator();
                 var petsEnumerator = pets.GetEnumerator();
@@ -56,9 +95,9 @@ namespace CSharpAdvanceDesignTests
                 while (petsEnumerator.MoveNext())
                 {
                     var pet = petsEnumerator.Current;
-                    if (pet.Owner.Equals(employee))
+                    if (petKeySelector(pet).Owner.Equals(employeeKeySelector(employee)))
                     {
-                        yield return Tuple.Create(pet.Owner.FirstName, pet.Name);
+                        yield return resultSelector(employeeKeySelector(employee), petKeySelector(pet));
                     }
                 }
                 petsEnumerator.Reset();

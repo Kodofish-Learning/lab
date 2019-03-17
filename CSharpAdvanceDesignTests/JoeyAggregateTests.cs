6c44193bd385089c533ead03d9dc87a266318fa8
kodofish Chang <kodofish@gmail.com>
2019-03-17 15:33:30 +0800

    - Implement: Aggregate

--- CSharpAdvanceDesignTests/JoeyAggregateTests.cs
+++ CSharpAdvanceDesignTests/JoeyAggregateTests.cs
@@ -1,13 +1,49 @@
-﻿using NUnit.Framework;
+﻿using System;
 using System.Collections.Generic;
+using NUnit.Framework;
 
 namespace CSharpAdvanceDesignTests
 {
     [TestFixture]
     public class JoeyAggregateTests
     {
+        private string JoeyAggregate
+        (IEnumerable<int> drawlingList, 
+            decimal balance, 
+            Func<decimal, decimal, decimal> func, 
+            Func<decimal, string> selector)
+        {
+            var seed = balance;
+            var drawlingListEnumerator = drawlingList.GetEnumerator();
+            while (drawlingListEnumerator.MoveNext())
+            {
+                var drawling = drawlingListEnumerator.Current;
+                seed = func(seed, drawling);
+            }
+
+            return selector(seed);
+        }
+
+//        [Test]
+//        public void drawling_money_that_balance_have_to_be_positive()
+//        {
+//            var balance = 100.91m;
+//
+//            var drawlingList = new List<int>
+//            {
+//                30, 80, 20, 40, 25
+//            };
+//
+//            var actual = JoeyAggregate(drawlingList, balance,
+//                (seed, drawling) => seed - (seed >= drawling ? drawling : 0));
+//
+//            var expected = 10.91m;
+//
+//            Assert.AreEqual(expected, actual);
+//        }
+        
         [Test]
-        public void drawling_money_that_balance_have_to_be_positive()
+        public void drawling_money_that_balance_have_to_be_positive2()
         {
             var balance = 100.91m;
 
@@ -16,26 +52,12 @@ namespace CSharpAdvanceDesignTests
                 30, 80, 20, 40, 25
             };
 
-            var actual = JoeyAggregate(drawlingList, balance);
+            var actual = JoeyAggregate(drawlingList, balance,
+                (seed, drawling) => seed - (seed >= drawling ? drawling : 0), seed1 => seed1.ToString());
 
-            var expected = 10.91m;
+            var expected = "10.91";
 
             Assert.AreEqual(expected, actual);
-        }
-
-        private decimal JoeyAggregate(IEnumerable<int> drawlingList, decimal balance)
-        {
-            var drawlingListEnumerator = drawlingList.GetEnumerator();
-            while (drawlingListEnumerator.MoveNext())
-            {
-                var drawling = drawlingListEnumerator.Current;
-                if (balance > drawling)
-                {
-                    balance -= drawling;
-                }
-            }
-
-            return balance;
         }
     }
 }

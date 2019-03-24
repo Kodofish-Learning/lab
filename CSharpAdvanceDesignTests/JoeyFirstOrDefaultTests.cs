9cc170e91c9e5619861eb733212f1f1d80d67143
kodofish Chang <kodofish@gmail.com>
2019-03-24 18:14:19 +0800

    FirstOrDefault(), Implement 2nd test

--- CSharpAdvanceDesignTests/JoeyFirstOrDefaultTests.cs
+++ CSharpAdvanceDesignTests/JoeyFirstOrDefaultTests.cs
@@ -3,6 +3,7 @@ using NUnit.Framework;
 using NUnit.Framework.Internal;
 using System;
 using System.Collections.Generic;
+using FluentAssertions;
 
 namespace CSharpAdvanceDesignTests
 {
@@ -17,6 +18,33 @@ namespace CSharpAdvanceDesignTests
             var actual = JoeyFirstOrDefault(employees);
 
             Assert.IsNull(actual);
+        }
+
+        [Test]
+        public void get_employee_when_employees_is_not_empty()
+        {
+                var employees = new List<string>()
+                {"Fish"};
+
+        var actual = JoeyFirstOrDefault(employees);
+
+        Assert.IsNotNull(actual);
+        actual.Should().Equals("Fish");
+        }
+
+        private string JoeyFirstOrDefault(List<string> employees)
+        {
+            var sourceEnumerator = employees.GetEnumerator();
+            while (sourceEnumerator.MoveNext())
+            {
+                var current = sourceEnumerator.Current;
+                if (current != null)
+                {
+                    return current;
+                }
+            }
+
+            return null;
         }
 
         private Employee JoeyFirstOrDefault(IEnumerable<Employee> employees)

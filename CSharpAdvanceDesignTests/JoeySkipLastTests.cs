7ba32649a4fe0d61f54c8cd39d274c4a3418e97c
kodofish Chang <kodofish@gmail.com>
2019-03-25 01:21:48 +0800

    Add SkipLast()

---
+++ CSharpAdvanceDesignTests/JoeySkipLastTests.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using ExpectedObjects;
+using NUnit.Framework;
+
+namespace CSharpAdvanceDesignTests
+{
+    public class JoeySkipLastTest
+    {
+        [Test]
+        public void skip_last_2()
+        {
+            var numbers = new[] { 10,20,30,40,50,60};
+            var expected = new[]{10,20,30,40};
+            var actual = JoeySkipLast(numbers, 2);
+            actual.ToExpectedObject().ShouldMatch(expected);
+
+        }
+
+        private IEnumerable<int> JoeySkipLast(IEnumerable<int> numbers, int count)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
